edf1a3b104c14484b53cb529c045029f70506639
leah0913 <46901930+leah0913@users.noreply.github.com>
2019-08-06 01:21:19 -0400

    First Upload

---
+++ assets/data/profileData.ts
@@ -0,0 +1,9 @@
+import { Jeah } from '../../app/jeah';
+
+export const JEAH: Jeah = {
+    sNumber: '991509706',
+    sName: 'Leah Kim',
+    sLoginName: 'jeah',
+    sCampus: 'Trafalgar',
+    aTitle: 'Food Items'
+};
